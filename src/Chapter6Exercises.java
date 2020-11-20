816686ef9acb0c91b93ed0aee93530f255da95c7
Igor <houwat@gmail.com>
2020-11-20 18:43:44 -0500

    Chapter 6 - experimenting with Swing GUI

---
+++ src/Chapter6Exercises.java
@@ -0,0 +1,8 @@
+//Chapter 6 - Introduction to GUI programming
+
+import javax.swing.JOptionPane;
+public class Chapter6Exercises {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog( null, "Hello World!" );
+    }
+}
